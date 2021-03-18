02692b8045bf090367fd149418d81c644b26622f
Joshua Schnabel <git@joshua-schnabel.de>
2021-03-19 00:25:06 +0100

    Zwischenstand

---
+++ src/main/java/de/joshuaschnabel/anthill/domain/_validator/ValidationOperation.java
@@ -0,0 +1,25 @@
+package de.joshuaschnabel.anthill.domain._validator;
+
+public class ValidationOperation<O> {
+
+	private Validator<O> validator;
+
+	public ValidationOperation(Validator<O> validator) {
+		this.validator = validator;
+	}
+
+	public ValidatorCondition<O> and() {
+		this.validator.and();
+		return new ValidatorCondition<O>(validator);
+	}
+
+	public ValidatorCondition<O> or() {
+		this.validator.or();
+		return new ValidatorCondition<O>(validator);
+	}
+
+	public Boolean xyz(String string) {
+		return this.validator.execute();
+	}
+
+}
